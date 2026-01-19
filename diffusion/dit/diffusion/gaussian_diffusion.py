e717e0d33ba756be369c99d8964ffd0efd24aa35
tg.ahn <timothy3017@gmail.com>
2026-01-20 03:12:27 +0900

    feat: dit fk steering

--- diffusion/dit/diffusion/gaussian_diffusion.py
+++ diffusion/dit/diffusion/gaussian_diffusion.py
@@ -510,6 +510,113 @@ class GaussianDiffusion:
                 yield out
                 img = out["sample"]
 
+    def p_sample_loop_fkd(
+        self,
+        model,
+        shape,
+        noise=None,
+        clip_denoised=True,
+        denoised_fn=None,
+        cond_fn=None,
+        model_kwargs=None,
+        device=None,
+        progress=False,
+        fkd=None,
+        reward_fn_args: dict = {},
+    ):
+        """
+        Generate samples from the model.
+        :param model: the model module.
+        :param shape: the shape of the samples, (N, C, H, W).
+        :param noise: if specified, the noise from the encoder to sample.
+                      Should be of the same shape as `shape`.
+        :param clip_denoised: if True, clip x_start predictions to [-1, 1].
+        :param denoised_fn: if not None, a function which applies to the
+            x_start prediction before it is used to sample.
+        :param cond_fn: if not None, this is a gradient function that acts
+                        similarly to the model.
+        :param model_kwargs: if not None, a dict of extra keyword arguments to
+            pass to the model. This can be used for conditioning.
+        :param device: if specified, the device to create the samples on.
+                       If not specified, use a model parameter's device.
+        :param progress: if True, show a tqdm progress bar.
+        :return: a non-differentiable batch of samples.
+        """
+        final = None
+        for sample in self.p_sample_loop_fkd_progressive(
+            model,
+            shape,
+            noise=noise,
+            clip_denoised=clip_denoised,
+            denoised_fn=denoised_fn,
+            cond_fn=cond_fn,
+            model_kwargs=model_kwargs,
+            device=device,
+            progress=progress,
+            fkd=fkd,
+            reward_fn_args=reward_fn_args,
+        ):
+            final = sample
+        return final["sample"]
+
+    def p_sample_loop_fkd_progressive(
+        self,
+        model,
+        shape,
+        noise=None,
+        clip_denoised=True,
+        denoised_fn=None,
+        cond_fn=None,
+        model_kwargs=None,
+        device=None,
+        progress=False,
+        fkd=None,
+        reward_fn_args: dict = {},
+    ):
+        """
+        Generate samples from the model and yield intermediate samples from
+        each timestep of diffusion.
+        Arguments are the same as p_sample_loop().
+        Returns a generator over dicts, where each dict is the return value of
+        p_sample().
+        """
+        if device is None:
+            device = next(model.parameters()).device
+        assert isinstance(shape, (tuple, list))
+        if noise is not None:
+            img = noise
+        else:
+            img = th.randn(*shape, device=device)
+        indices = list(range(self.num_timesteps))[::-1]
+
+        if progress:
+            # Lazy import so that we don't depend on tqdm.
+            from tqdm.auto import tqdm
+
+            indices = tqdm(indices)
+
+        for ii ,i in enumerate(indices):
+            t = th.tensor([i] * shape[0], device=device)
+            with th.no_grad():
+                out = self.p_sample(
+                    model,
+                    img,
+                    t,
+                    clip_denoised=clip_denoised,
+                    denoised_fn=denoised_fn,
+                    cond_fn=cond_fn,
+                    model_kwargs=model_kwargs,
+                )
+                if fkd is not None:
+                    out["sample"], current_pop_images = fkd.resample(
+                    sampling_idx=ii+1, # note first proposal to T-1 already done  # TODO: check
+                    latents=out["sample"],
+                    x0_preds=out["pred_xstart"],
+                    reward_fn_args=reward_fn_args,
+                )
+                yield out
+                img = out["sample"]
+
     def ddim_sample(
         self,
         model,
--- diffusion/dit/model.py
+++ diffusion/dit/model.py
@@ -1,8 +1,15 @@
+import warnings
+
+from typing import Optional, Union
+from copy import deepcopy
+
 import torch
+import numpy as np
+
 from .models import DiT_models
 from .diffusion import create_diffusion
 from diffusers.models import AutoencoderKL
-
+from ..edm.fkd import FKD, FKD_ARGS_DEFAULTS
 
 class DiT(torch.nn.Module):
     def __init__(
@@ -71,3 +78,83 @@ class DiT(torch.nn.Module):
         samples = self.vae.decode(samples / 0.18215).sample
 
         return samples.to(torch.float32), y
+
+
+    def sample_fk_steering(
+        self,
+        latents=None,
+        class_label:  Optional[Union[int, list]] = None,
+        reward_fn=None,
+        reward_fn_args: dict = {},
+        fkd_args: dict = FKD_ARGS_DEFAULTS,
+    ):
+        device = next(iter(self.model.parameters())).device
+        # dtype = dtype_mapping[self.dtype]
+
+        fkd_args = deepcopy(fkd_args)
+        self.batch_size = fkd_args["num_particles"]
+        if fkd_args["resampling_t_start"] == 0:
+            # raise NotImplementedError("resampling_t_start==0 is not implemented")
+            warnings.warn("fkd_args['resampling_t_start']=0 is not implemented, setting to 1")
+            fkd_args["resampling_t_start"] = 1
+        if fkd_args["resampling_t_end"] is None:
+            fkd_args["resampling_t_end"] = self.num_steps
+
+        fkd_args["time_steps"] = self.num_steps
+
+        if latents is None:
+            latents = torch.randn(self.batch_size, self.model.in_channels, self.latent_size, self.latent_size, device=device)
+    
+
+        if class_label is None:
+            class_label = torch.randint(self.num_classes, (), device="cpu").item()
+        if isinstance(class_label, int):
+            y = torch.full((self.batch_size,), class_label, device=device, dtype=torch.long)
+        
+
+        reward_fn_args["targets"] = y
+        # Set up for FK-steering
+        reward_fn_args["prompts"] = class_label
+
+        if fkd_args is not None and fkd_args.get("use_smc", False):
+            latent_to_decode_fn_ = fkd_args.get("latent_to_decode_fn", None)
+            def latent_to_decode_fn(latents):
+                samples = self.vae.decode(latents / 0.18215).sample
+
+                if latent_to_decode_fn_ is not None:
+                    samples = latent_to_decode_fn_(samples)
+                return samples
+            fkd_args["latent_to_decode_fn"] = latent_to_decode_fn
+
+            fkd = FKD(
+                reward_fn=reward_fn,
+                **fkd_args,
+            )
+        else:
+            fkd = None
+
+    
+        # Setup classifier-free guidance:
+        model_kwargs = dict(y=y)
+        sample_fn = self.model.forward
+
+        # Sample images:
+        samples = self.diffusion.p_sample_loop_fkd(
+            sample_fn, latents.shape, latents, clip_denoised=False, model_kwargs=model_kwargs, progress=False, device=device, fkd=fkd, reward_fn_args=reward_fn_args
+        )
+        
+        samples = self.vae.decode(samples / 0.18215).sample
+
+        if fkd is not None:
+            population_images = latent_to_decode_fn_(samples)
+            rewards = fkd.reward_fn(population_images, **reward_fn_args).cpu().tolist()
+            max_index = np.argmax(rewards)
+        else:
+            rewards = [0] * self.batch_size
+            max_index = 0  # if not using FKD, just return the first sample
+
+        return (
+            samples[max_index].unsqueeze(0).to(torch.float32),
+            y[max_index].unsqueeze(0),
+            rewards[max_index],
+        )
